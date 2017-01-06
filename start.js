c1ecfbf9dcb1625eb678bd9e3de72ee834fbdc93
FanC <depressionhysteria@gmail.com>
2017-01-06 19:36:04 +0800

    1/6

---
+++ start.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function()
+{
+
+
+
+
+$(".first").hover(
+function(){
+	$(".firstimg").css("display","inline");
+},
+function(){
+	$(".firstimg").css("display","none");
+});
+
+
+
+$(".design").hover(
+function(){
+	$(".designimg").css("display","inline");
+},
+function(){
+	$(".designimg").css("display","none");
+});
+
+
+$(".acheived").hover(
+function(){
+	$(".acheivedimg").css("display","inline");
+},
+function(){
+	$(".acheivedimg").css("display","none");
+});
+
+
+});
